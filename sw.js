932e42852ec7a99cf1a383f978793e7cab3de6b6
defamuk <sagnik.de2008@gmail.com>
2026-03-05 11:10:46 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fueltrack-v3';
+const CACHE_NAME = 'fueltrack-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
